0674ae4ea97f399bf2d8c30b02e6c7d32f8e8b9a
ctyxy <chenty2017@126.com>
2018-08-03 23:50:55 +0800

    2018.8.3 gui tkinter study

--- tkinterstudy2.py
+++ tkinterstudy2.py
@@ -3,7 +3,7 @@ import time
 base=tkinter.Tk()
 base.wm_title("study2")
 base.minsize(600,500)
-
+base.maxsize(700,600)
 def show_lab():
       global base
       lab=tkinter.Label(base,text=time.ctime())
