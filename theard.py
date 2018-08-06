23b1b9bf0284dcd745554605bda3e9017f9947ce
ctyxy <chenty2017@126.com>
2018-08-06 22:46:24 +0800

    2018.8.6  update thread.py

--- theard.py
+++ theard.py
@@ -1,28 +1,31 @@
 import threading
-import time
+from time import sleep, ctime
 
 # 时间循环1：开始到结束4秒
-def loop1():
-    print("Start loop1, time: ", time.ctime())
-    time.sleep(2)
-    print("End Loop1,time: ", time.ctime())
+def loop(i):
+      print("Start loop ", i, ", time: ", ctime())
+      sleep(i)
+      print("End Loop ", i, " , time: ", ctime())
 
 #时间循环2：开始到结束3秒
-def loop2():
-    print("Start Loop2,time ",time.ctime())
-    time.sleep(3)
-    print("End Loop2,time: ",time.ctime())
 
-#设置loop2为守护线程，loop1线程和主线程都结束，Loop2线程才会被结束
 
-print("Start time:", time.ctime())
-t1=threading.Thread(target=loop1)
+print("Start time:", ctime())
+t1 = threading.Thread(target=loop, args=(3,))
 t1.start()
-t2=threading.Thread(target=loop2)
+t2 = threading.Thread(target=loop, args=(2,))
+# t2.setDaemon(True)
+t2.start()
+print()
+print("All done at ", ctime())
+
+# 设置loop2为守护线程，loop(1)线程和主线程都结束，Loop（2)线程才会被结束
+
+print("Start time:", ctime())
+t1 = threading.Thread(target=loop, args=(1,))
+t1.start()
+t2 = threading.Thread(target=loop, args=(2,))
 t2.setDaemon(True)
 t2.start()
 print()
-print("All done at ",time.ctime())
-
-
-
+print("All done at ", ctime())
